2c1eb05a4c872f992ee2eff4c782d2b4fda393ba
Adam Sharp <adam@sharplet.me>
2017-03-17 17:46:12 -0400

    User adds new todo and sees it on the homepage

--- todos/app/controllers/todos_controller.rb
+++ todos/app/controllers/todos_controller.rb
@@ -1,4 +1,25 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  def todo_params
+    todo = params.require(:todo)
+    {
+      title: todo.require(:title),
+    }
   end
 end
